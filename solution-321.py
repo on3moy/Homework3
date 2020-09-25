e36218ebde6247ef7dc4371dbd4bb6bdaa86fe2d
Moy Patel <moy.patel01@student.csulb.edu>
2020-09-24 23:11:32 -0700

    Grammer check

--- solution-321.py
+++ solution-321.py
@@ -1,7 +1,7 @@
 """
 Exercise 3.21 (Calculate Change Using Fewest Number of Coins)
 Write a script that inputs a purchase price of a dollar or less for an item. Assume the purchaser pays with a dollar bill.
-Assume the purchaser  pays with a dollar bill. Detemrine the amount of change the cashier should give back to the purchaser.
+Detemrine the amount of change the cashier should give back to the purchaser.
 Display the change using the fewest number of pennies, nickels, dimes and quarters. For example, if the purhcaser is due 73 cents in change,
 the script would output:
 Your change is:
@@ -9,21 +9,6 @@ Your change is:
 2 dimes
 3 pennies
 """
-
-# Assign Variables
-customer_pay = 1
-quarters = .25
-dimes = .1
-nickels = .5
-pennies = .01
-
-# Ensure user inputs a decimal form 0-1
-while True:
-    purchase_price = input('What is the purchase price?: (Must be greater than $0 and less than $1)')
-    if not purchase_price.isalpha():
-        if float(purchase_price) > 0 and float(purchase_price)<=1:
-            purchase_price = float(purchase_price)
-            break
 
 # Assign Variables
 customer_pay = 1
